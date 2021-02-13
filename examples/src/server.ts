d03865c12369a210dc33af844ecc43bdedb0724b
Ahmed Elywa <ahmed.elywa@icloud.com>
2021-02-13 23:45:22 +0200

    fix example

--- examples/src/server.ts
+++ examples/src/server.ts
@@ -2,13 +2,15 @@ import { ApolloServer } from 'apollo-server'
 import {
   transformSchemaFederation,
   printTransformedSchema,
-} from '@spantree/nexus-federation'
+} from 'nexus-federation-plugin'
 import { createContext } from './context'
 import { nexusSchema } from './nexusSchema'
+import { ApolloServerPluginInlineTraceDisabled } from 'apollo-server-core'
 
 const server = new ApolloServer({
   schema: transformSchemaFederation(nexusSchema),
   context: createContext,
+  plugins: [ApolloServerPluginInlineTraceDisabled()],
 })
 
 server.listen().then(({ url }) => {
